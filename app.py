d9a631dac094bc8e44a42da17b23ba3da7e32216
William Baye <williambaye22@gmail.com>
2024-01-02 23:29:03 +0100

    initialise webhook

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from flask import Flask, request, jsonify, render_template
 from models import  pd, nd, liste_communes, all_departements, communes_for_map_a, francemetropole, path_abstentions
 #from models_geojson import  pd, nd, map_1, liste_communes, all_departements, communes_for_map
 import json
+import git
 app = Flask(__name__, template_folder= "./processed/html_files/")
 
-
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/ousontlesabstentions/mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route('/', methods = ['GET'])
 def trial():
