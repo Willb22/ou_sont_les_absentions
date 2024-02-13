efca1f48c7728646d60c0a052a963a80feadde60
William Baye <williambaye22@gmail.com>
2024-02-13 19:49:03 +0100

    ADD remotely connect to aws postgresql database

--- models.py
+++ models.py
@@ -8,9 +8,11 @@ import psycopg2
 from sqlalchemy import create_engine, MetaData, Table, Column, String, Float, Integer, select, distinct
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import registry
+from datetime import datetime
+from map_configuration import dbmapconfig
 
 database_name = 'ou_sont_les_abstentions'
-from datetime import datetime
+
 
 now = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
@@ -31,12 +33,12 @@ class User:
 
 
 class Table_queries:
-	def __init__(self, path_abstentions, path_paris, table_name):
+	def __init__(self, path_abstentions, path_paris, table_name, query_aws_table):
 		self.path_abstentions = path_abstentions
 		self.path_paris = path_paris
 		self.table_name =table_name
-		#self.df = self.prepare_df(self.path_abstentions)
 		self.static_francemetropole=True
+		self.query_aws_table=query_aws_table
 
 
 	def define_mapper(self):
@@ -72,7 +74,7 @@ class Table_queries:
 		mapper_registry = registry()
 		mapper_registry.map_imperatively(User, france_pres_2017)
 
-	def get_credentials(self):
+	def get_credentials(self, use_aws):
 		"""
 		Return user, password, host and port for database and table connection
 
@@ -86,9 +88,14 @@ class Table_queries:
 		    user, password, host and port
 		"""
 		user = 'postgres'
-		passw = os.environ.get('PASSPOSTGRES')
-		host = '127.0.0.1'
-		port = os.environ.get('PORT_POSTGRESQL')
+		if use_aws:
+			host = 'ec2-54-173-241-113.compute-1.amazonaws.com' # Public IPv4 DNS, contains elastic IP to AWS instance
+			port = os.environ.get('PORT_POSTGRESQL_AWS')
+			passw = os.environ.get('PASSPOSTGRES')
+		else:
+			host = '127.0.0.1'
+			port = os.environ.get('PORT_POSTGRESQL')
+			passw = os.environ.get('PASSPOSTGRES')
 
 		return user, passw, host, port
 
@@ -104,7 +111,7 @@ class Table_queries:
         -------
         connction and cursor class
         """
-		user, passw, host, port = self.get_credentials()
+		user, passw, host, port = self.get_credentials(self.query_aws_table)
 
 		# establishing the connection
 		conn = psycopg2.connect(
@@ -127,7 +134,7 @@ class Table_queries:
         -------
         sqlalchemy Engine instance and connection
         """
-		user, passw, host, port = self.get_credentials()
+		user, passw, host, port = self.get_credentials(self.query_aws_table)
 
 		conn_string = f'postgresql://{user}:{passw}@{host}:{port}/{database_name}'
 		engine = create_engine(conn_string)
@@ -242,8 +249,8 @@ class Table_queries:
 		return res
 
 class Queries_france2017(Table_queries):
-	def __init__(self, path_abstentions, path_paris):
-		super().__init__(path_abstentions, path_paris, table_name = 'france_pres_2017')
+	def __init__(self, path_abstentions, path_paris, query_aws_table):
+		super().__init__(path_abstentions, path_paris, table_name = 'france_pres_2017', query_aws_table=query_aws_table)
 		self.conn_orm, self.db = self.connect_orm()
 		Session = sessionmaker(bind=self.db)
 		self.session = Session()
@@ -313,7 +320,6 @@ class Queries_france2017(Table_queries):
 		list_data  = [list(row) for row in data_chunks]
 		dict_data = self.create_dict_for_map(list_data, call_col)
 		res = KeplerGl(height=500, data={"data_1": dict_data}, config=dbmapconfig)
-		#res = KeplerGl(height=500, data={"data_1": dict_data}) # default config applied
 		return res
 
 
@@ -363,191 +369,6 @@ class Queries_france2022(Table_queries):
 
 		return df_with_paris
 
-process_france2017 = Queries_france2017(path_abstentions_france2017, path_paris_france2017)
+process_france2017 = Queries_france2017(path_abstentions_france2017, path_paris_france2017, query_aws_table=True)
 #france2022 = Process_france2022(path_abstentions_france2022, path_paris_france2022, table_name='france_pres_2022')
 
-
-colorscheme = [
-					  "#5A1846",
-					  "#900C3F",
-					  "#C70039",
-					  "#E3611C",
-					  "#F1920E",
-					  "#FFC300"
-					]
-
-#colorscheme = ['FFFF00', 'FFCC00', 'FF9900', 'FF6600', 'FF3300', 'FF0000']
-colorscheme = ['#FFFF00', '#FFCC00', '#FF9900', '#FF6600', '#FF3300', '#FF0000']
-#colorscheme = ['#f2e600', '#e6cc00', '#d9b300', '#cc9900', '#c08000', '#b36600', '#a64d00', '#993300', '#8d1a00', '#800000']
-#colorscheme = ['#FFE6E6', '#FFCCCC', '#FFFB2B2', '#FF9999', '#FF8080', '#FF1D1D', '#FF1919','CC0000', '#990000', '#660000']
-colorscheme = ['#ffe6e6', '#ffcccc', '#ff9999', '#ff4d4d', '#ff1a1a', '#cc0000','#CC0000', '#800000', '#330000']
-
-
-
-#dbmapconfig = _mapconfig.copy()
-#dbmapconfig["config"]["visState"]["interactionConfig"]["tooltip"]["fieldsToShow"]["data_1"][0]["name"] = "Pourcentage Absentions"
-dbmapconfig = {
-	  "version": "v1",
-	  "config": {
-		"visState": {
-		  "filters": [],
-		  "layers": [
-			{
-			  "id": "c9y8sgp",
-			  "type": "point",
-			  "config": {
-				"dataId": "data_1",
-				"label": "Point",
-				"color":[255,203,153],
-			    "highlightColor":[252,242,26,255],
-				"columns": {
-				  "lat": "latitude",
-				  "lng": "longitude",
-				},
-				"isVisible": True,
-				"visConfig": {
-				  "radius": 17.2,
-				  "fixedRadius": False,
-				  "opacity": 0.8,
-				  "outline": False,
-				  "thickness": 2,
-				  "strokeColor": None,
-				  "colorRange": {
-					"name": "Global Warming",
-					"type": "sequential",
-					"category": "Uber",
-					"colors": colorscheme
-				  },
-				  "strokeColorRange": {
-					"name": "Global Warming",
-					"type": "sequential",
-					"category": "Uber",
-					"colors": colorscheme
-				  },
-				  "radiusRange": [
-					0,
-					50
-				  ],
-				  "filled": True
-				},
-				"hidden": False,
-				"textLabel": [
-				  {
-					"field": None,
-					"color": [
-					  255,
-					  255,
-					  255
-					],
-					"size": 18,
-					"offset": [
-					  0,
-					  0
-					],
-					"anchor": "start",
-					"alignment": "center", "outlineWidth":0,"outlineColor":[255,0,0,255],"background":False,"backgroundColor":[0,0,200,255]
-				  }
-				]
-			  },
-			  "visualChannels": {
-				"colorField": {
-				  "name": "Pourcentage Absentions",
-				  "type": "float"
-				},
-				"colorScale": "quantile",
-				"strokeColorField": "Pourcentage Absentions",
-				"strokeColorScale": "quantile",
-				"sizeField": {
-				  "name": "Inscrits",
-				  "type": "integer"
-				},
-				"sizeScale": "linear"
-			  }
-			}
-		  ],
-		  "interactionConfig": {
-			"tooltip": {
-			  "fieldsToShow": {
-				"data_1": [
-
-				  {
-					"name": "Pourcentage Absentions",
-					"format": None
-				  },
-				  {
-					"name": "Abstentions",
-					"format": None
-				  },
-				  {
-					"name": "Inscrits",
-					"format": None
-				  },
-				  				  {
-					"name": "Libellé du département",
-					"format": None
-				  },
-				  {
-					"name": "Libellé de la commune",
-					"format": None
-				  },
-				  {
-					"name": "Adresse complète",
-					"format": None
-				  }
-				]
-			  },
-			  "compareMode": False,
-			  "compareType": "absolute",
-			  "enabled": True
-			},
-			"brush": {
-			  "size": 0.5,
-			  "enabled": False
-			},
-			"geocoder": {
-			  "enabled": False
-			},
-			"coordinate": {
-			  "enabled": False
-			}
-		  },
-		  "layerBlending": "normal",
-		  "splitMaps": [],
-		  "animationConfig": {
-			"currentTime": None,
-			"speed": 1
-		  }
-		},
-		"mapState": {
-		  "bearing": 0,
-		  "dragRotate": False,
-		  "latitude": 46.82432869985292,
-		  "longitude": 0.6931065883195648,
-		  "pitch": 0,
-		  "zoom": 5.279017859889528,
-		  "isSplit": False
-
-		},
-		"mapStyle": {
-		  "styleType": "dark",
-		  "topLayerGroups": {},
-		  "visibleLayerGroups": {
-			"label": True,
-			"road": True,
-			"border": False,
-			"building": True,
-			"water": True,
-			"land": True,
-			"3d building": False
-		  },
-		  "threeDBuildingColor": [
-			9.665468314072013,
-			17.18305478057247,
-			31.1442867897876
-		  ],
-		  "backgroundColor":[0,0,0],
-		  "mapStyles": {},
-
-		}
-	  }
-	}
