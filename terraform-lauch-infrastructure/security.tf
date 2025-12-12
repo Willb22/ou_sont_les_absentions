5b847756bdef615e8e6b88149958eb6a9b019674
William Baye <williambaye22@gmail.com>
2025-12-12 18:57:36 +0100

    FIX ec2 allow inbound ssh from anywhere

--- terraform-lauch-infrastructure/security.tf
+++ terraform-lauch-infrastructure/security.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
