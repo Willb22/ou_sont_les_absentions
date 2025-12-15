852f22c266645c2029c3374cc34de14c6df66167
William Baye <williambaye22@gmail.com>
2025-12-15 22:19:45 +0100

    ADD open port 5000 on ec2

--- terraform-lauch-infrastructure/security.tf
+++ terraform-lauch-infrastructure/security.tf
@@ -14,6 +14,17 @@ resource "aws_security_group" "ssh_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  # Dev env via port 5000
+  ingress {
+    description = "Web App Port 5000"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
