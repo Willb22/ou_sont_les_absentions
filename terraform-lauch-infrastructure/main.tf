28a1cdd835b47267ff17a07e577caa78bd9b7097
William Baye <williambaye22@gmail.com>
2025-12-15 22:17:25 +0100

    FIX AppArmor blocks and use Ubuntu 22

--- terraform-lauch-infrastructure/main.tf
+++ terraform-lauch-infrastructure/main.tf
@@ -1,14 +1,14 @@
 
 
 
-# Get latest Ubuntu 24.04 LTS (Noble) AMI from Canonical
-data "aws_ami" "ubuntu_24_04" {
+# Get Ubuntu 22
+data "aws_ami" "ubuntu_22" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -23,15 +23,15 @@ data "aws_ami" "ubuntu_24_04" {
 }
 
 
-
 resource "aws_instance" "ubuntu_ec2" {
-  ami           = data.aws_ami.ubuntu_24_04.id
+  ami           = data.aws_ami.ubuntu_22.id
   instance_type = "t3.micro"
 
   # AWS key pair for ssh and scp
   key_name = "first_ec2"
 
   tags = {
-    Name = "Ubuntu24-EC2"
+    Name = "Ubuntu22"
   }
+
 }
