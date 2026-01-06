1cc7df20374d1e09b64a7831e31dba365cb82011
William Baye <williambaye22@gmail.com>
2026-01-06 23:33:09 +0100

    ADD terraform git clone specific branch

--- terraform-lauch-infrastructure/git_clone_project.sh
+++ terraform-lauch-infrastructure/git_clone_project.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-su - ubuntu -c "git clone https://github.com/Willb22/ou_sont_les_absentions.git /home/ubuntu/ou_sont_les_absentions"
+su - ubuntu -c "git clone --branch dev https://github.com/Willb22/ou_sont_les_absentions.git /home/ubuntu/ou_sont_les_absentions"
