02a3629ef3193375abc2c6f2d2b3166fe48ef390
William Baye <williambaye22@gmail.com>
2026-01-15 22:06:30 +0100

    REFAC terraform ammend ec2 name

--- terraform-lauch-infrastructure/main.tf
+++ terraform-lauch-infrastructure/main.tf
@@ -26,17 +26,18 @@ data "aws_ami" "ubuntu_22" {
 resource "aws_instance" "ubuntu_ec2" {
   ami           = data.aws_ami.ubuntu_22.id
   instance_type = "t3.micro"
-# Specify Security group for all ports on EC2.
- vpc_security_group_ids = [
-   aws_security_group.inbound_outbound_access.id
- ]
+  # Specify Security group for all ports on EC2.
+  vpc_security_group_ids = [
+    aws_security_group.inbound_outbound_access.id
+  ]
 
   # AWS key pair for ssh and scp
   key_name = "first_ec2"
 
   tags = {
-    Name = "Ubuntu22"
+    Name = "dev"
   }
+  #associate_public_ip_address = false # Disable ephemeral public IPs explicitly
 
   root_block_device {
     volume_size = 35       # <-- 35 GB
@@ -47,23 +48,25 @@ resource "aws_instance" "ubuntu_ec2" {
   #!/bin/bash
   #set -e #Comment here to let script pursue after a failed line
 
-
   ${file("${path.module}/install_docker_docker_compose.sh")}
   ${file("${path.module}/git_clone_project.sh")}
 
   cat << 'ENVEOF' > /home/ubuntu/ou_sont_les_absentions/.env
   ${file("../.env")}
   ENVEOF
-  
+
   # Run docker compose
   ${file("${path.module}/run_docker_compose.sh")}
   # Prepare web environment
   ${file("${path.module}/install_nginx.sh")}
-  
+
   sudo cat << 'NGINXEOF' > /etc/nginx/nginx.conf
   ${file("nginx_dev_conf.txt")}
   NGINXEOF
   ${file("${path.module}/certbot_ca_certificate.sh")}
+
+
+
 
   EOF
 # String interpolation
