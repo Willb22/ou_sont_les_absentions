f122541801fb45fb29ebf24597c3bc00e8f3b693
William Baye <williambaye22@gmail.com>
2026-01-23 23:13:27 +0100

    FIX path to scripts

--- scripts_ec2_deployment/bootstrap.sh
+++ scripts_ec2_deployment/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #set -e #Comment here to let script pursue after a failed line
-
+CURRENT_DIR="/home/ubuntu/ou_sont_les_absentions/scripts_ec2_deployment"
 # Ensure env is loaded
 export $(grep -v '^#' /home/ubuntu/ou_sont_les_absentions/.env | xargs)
 
-./scripts/install_docker_docker_compose.sh
-./scripts/restore_backup_https_cert.sh
-./scripts/timer_clear_page_cache.sh
-./scripts/run_etl.sh
-./scripts/install_nginx.sh
-./scripts/certbot_ca_certificate.sh
-./scripts/run_webapp.sh
+sh "$CURRENT_DIR/install_docker_docker_compose.sh"
+sh "$CURRENT_DIR/restore_backup_https_cert.sh"
+sh "$CURRENT_DIR/timer_clear_page_cache.sh"
+sh "$CURRENT_DIR/run_etl.sh"
+sh "$CURRENT_DIR/install_nginx.sh"
+sh "$CURRENT_DIR/certbot_ca_certificate.sh"
+sh "$CURRENT_DIR/run_webapp.sh"
