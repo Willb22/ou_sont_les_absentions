f3fda0ecb969a0714484a3420cd70eaf1a10f5a5
William Baye <williambaye22@gmail.com>
2026-01-26 12:39:32 +0100

    FIX passing of environment variables on ec2 boot

--- scripts_ec2_deployment/bootstrap.sh
+++ scripts_ec2_deployment/bootstrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #set -e #Comment here to let script pursue after a failed line
 CURRENT_DIR="/home/ubuntu/ou_sont_les_absentions/scripts_ec2_deployment"
-# Ensure env is loaded
-export $(grep -v '^#' /home/ubuntu/ou_sont_les_absentions/.env | xargs)
+set -a      # auto-export variables so are available to child processes like aws
+source /home/ubuntu/ou_sont_les_absentions/.env
+set +a      # avoid auto-export of future variable defined after this line
 
 sh "$CURRENT_DIR/install_docker_docker_compose.sh"
-sudo sh "$CURRENT_DIR/restore_backup_https_cert.sh"
-sudo sh "$CURRENT_DIR/timer_clear_page_cache.sh"
-sudo sh "$CURRENT_DIR/run_etl.sh"
-sudo sh "$CURRENT_DIR/install_nginx.sh"
-sudo sh "$CURRENT_DIR/certbot_ca_certificate.sh"
-sudo sh "$CURRENT_DIR/run_webapp.sh"
+sudo -E sh "$CURRENT_DIR/restore_backup_https_cert.sh"
+sudo -E sh "$CURRENT_DIR/timer_clear_page_cache.sh"
+sudo -E sh "$CURRENT_DIR/run_etl.sh"
+sudo -E sh "$CURRENT_DIR/install_nginx.sh"
+sudo -E sh "$CURRENT_DIR/certbot_ca_certificate.sh"
+sudo -E sh "$CURRENT_DIR/run_webapp.sh"
--- scripts_ec2_deployment/install_nginx.sh
+++ scripts_ec2_deployment/install_nginx.sh
@@ -8,4 +8,12 @@ sudo ufw allow OpenSSH # Do not block ssh into ec2 for now
 #sudo ufw --force enable # Uncomment to enable Firewall
 sudo ufw reload
 #systemctl status nginx
-sudo bash -c 'envsubst < /home/ubuntu/ou_sont_les_absentions/web_app/nginx_conf/nginx.conf.tpl > /etc/nginx/nginx.conf'
+#embed single quotes inside single quotes for NAMESPACE variable
+sudo bash -c '
+  set -a
+  source /home/ubuntu/ou_sont_les_absentions/.env
+  set +a
+  envsubst '\''$NAMESPACE'\'' \
+    < /home/ubuntu/ou_sont_les_absentions/web_app/nginx_conf/nginx.conf.tpl \
+    > /etc/nginx/nginx.conf
+'
